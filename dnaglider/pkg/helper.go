6467cfd14ebd9aad4ef9ed240ac398540e91ef53
varogh <cyril.mattheydoret@gmail.com>
2020-10-07 02:11:45 +0200

    add helper functions to check errors and field names

---
+++ dnaglider/pkg/helper.go
@@ -0,0 +1,27 @@
+package pkg
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"regexp"
+)
+
+var fieldRegex = regexp.MustCompile("(GC|GCSKEW|ATSKEW|ENTRO|[0-9K]MER|)")
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func checkFields(fields []string) error {
+	var err error
+	for _, field := range fields {
+		if !fieldRegex.Match([]byte(field)) {
+			err = errors.New("Invalid field name")
+		}
+	}
+	return err
+}
